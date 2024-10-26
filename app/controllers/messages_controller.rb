940521829c909bc9d88f47147458c4f6015304e4
k-souta <souten8018@ezweb.ne.jp>
2024-10-26 21:30:01 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,23 @@
 class MessagesController < ApplicationController
   def index
-    @message= Message.new
+    @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
   end
 
-  def remember_created_at
+  def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
-      render :index, status: ;unproceessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
-
   private
 
   def message_params
-    params.require(:message).permit.merge(user_id: current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
